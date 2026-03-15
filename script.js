cc658c699a2f96ac960ca432e18455acccecfa9a
Vippatoori-SaiEshwar <saieshwar.992006@gmail.com>
2026-03-15 19:42:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document.getElementById("output").innerText = "Generating...";
 
 try{
 
-const response = await fetch("http://localhost:3000/generate",{
+const response = await fetch("https://ai-resume-bullet-generator.onrender.com/generate",{
 method:"POST",
 headers:{
 "Content-Type":"application/json"
